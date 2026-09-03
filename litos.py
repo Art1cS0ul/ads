30cf061fb7da82750c0a8230c8323139ddff3bc8
A <77242684+Art1cS0ul@users.noreply.github.com>
2026-09-03 17:33:58 -0300

    Calcula os litros gastos de acordo com a distancia

---
+++ litos.py
@@ -0,0 +1,7 @@
+Tempo=float(input("Digite o tempo da viagem em horas: "))
+Velocidade=float(input("Digite a velocidade média: "))
+
+Distancia=Tempo*Velocidade
+Litros=Distancia/12
+
+print("A quantidade de litros gastos foi:", Litros)
