f2460be5d102f566f526a5bbb55ed1673a1ca665
A <77242684+Art1cS0ul@users.noreply.github.com>
2026-09-03 17:28:29 -0300

    Calcula a idade daqui a um tempo

---
+++ idade.py
@@ -0,0 +1,8 @@
+AnoNasc=int(input("Digite o ano de nascimento: "))
+AnoAtual=int(input("Digite o ano atual: "))
+
+Idade=AnoAtual-AnoNasc
+Futuro=Idade+17
+
+print("Sua idade é:", Idade, "anos")
+print("Daqui a 17 anos você terá:", Futuro, "anos")
