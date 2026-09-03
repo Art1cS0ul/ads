a691a573bfee01cb07c9ba97ad77284ab73e05e9
A <77242684+Art1cS0ul@users.noreply.github.com>
2026-09-03 17:19:34 -0300

    Calcula a area de um traingulo

---
+++ tria.py
@@ -0,0 +1,11 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================== RESTART: C:/Users/gusta/3D Objects/quad.py ==================
+Digite seu salário com . ao invés de vírgula: 1.300
+O salário reajustado será de: 1.495 reais
+>>> 
+=================== RESTART: C:/Users/gusta/3D Objects/sal.py ==================
+Digite o valor da base do seu triângulo: 4
+Digite o valor da altura do seu triângulo: 5
+A base do seu triângulo será:  10.0
